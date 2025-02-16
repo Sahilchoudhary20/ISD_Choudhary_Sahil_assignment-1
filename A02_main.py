900c775dcf09bbbf20c2b9a40ac40dd846b4a4f1
schoudhary4 <schoudhary4@academic.rrc.ca>
2025-02-16 01:36:11 -0600

    updated the A02-main file

--- A02_main.py
+++ A02_main.py
@@ -1,40 +1,51 @@
 """
 Description: A client program written to verify correctness of 
-the BankAccount sub classes.
+the BankAccount sub-classes.
 """
 __author__ = "ACE Faculty"
 __version__ = "1.0.0"
-__credits__ = ""
+__credits__ = "Sahil Choudhary"
 
 # 1.  Import all BankAccount types using the bank_account package
 #     Import date from datetime
+from bank_account import *
+from datetime import date
 
+from bank_account.Investment_account import InvestmentAccount
+from bank_account.chequing_account import ChequingAccount
+from bank_account.savings_account import SavingsAccount
 
 # 2. Create an instance of a ChequingAccount with values of your 
 # choice including a balance which is below the overdraft limit.
-
+chequing_account = ChequingAccount(350, 350, -50, date(2024, 3, 25), -10, 0.05)
 
 # 3. Print the ChequingAccount created in step 2.
 # 3b. Print the service charges amount if calculated based on the 
 # current state of the ChequingAccount created in step 2.
-
+print(chequing_account)
+print(chequing_account.get_service_charges())
 
 # 4a. Use ChequingAccount instance created in step 2 to deposit 
 # enough money into the chequing account to avoid overdraft fees.
 # 4b. Print the ChequingAccount
 # 4c. Print the service charges amount if calculated based on the 
 # current state of the ChequingAccount created in step 2.
-
-
+try:
+    chequing_account.deposit(100)  
+    print(chequing_account)
+except Exception as e:
+    print('ERROR: ', e)
 print("===================================================")
+
 # 5. Create an instance of a SavingsAccount with values of your 
 # choice including a balance which is above the minimum balance.
-
+savings_account = SavingsAccount(350, 350, 350, date(2024, 3, 25), 30)
 
 # 6. Print the SavingsAccount created in step 5.
 # 6b. Print the service charges amount if calculated based on the 
 # current state of the SavingsAccount created in step 5.
-
+print(savings_account)
+print(savings_account.get_service_charges())
 
 # 7a. Use this SavingsAccount instance created in step 5 to withdraw 
 # enough money from the savings account to cause the balance to fall 
@@ -42,36 +53,50 @@ print("===================================================")
 # 7b. Print the SavingsAccount.
 # 7c. Print the service charges amount if calculated based on the 
 # current state of the SavingsAccount created in step 5.
-
-
-
+try:
+    savings_account.withdraw(98) 
+    print(savings_account)
+    print(savings_account.get_service_charges())
+except Exception as e:
+    print('ERROR: ', e)
 print("===================================================")
+
 # 8. Create an instance of an InvestmentAccount with values of your 
 # choice including a date created within the last 10 years.
-
+investment_account = InvestmentAccount(350, 350, 350, date(2024, 3, 25), 2)
 
 # 9a. Print the InvestmentAccount created in step 8.
 # 9b. Print the service charges amount if calculated based on the 
 # current state of the InvestmentAccount created in step 8.
-
+print(investment_account)
+print(investment_account.get_service_charges())
 
 # 10. Create an instance of an InvestmentAccount with values of your 
 # choice including a date created prior to 10 years ago.
-
+investment_account_2 = InvestmentAccount(350, 350, 350, date(2015, 3, 25), 2)
 
 # 11a. Print the InvestmentAccount created in step 10.
 # 11b. Print the service charges amount if calculated based on the 
 # current state of the InvestmentAccount created in step 10.
-
-
+print(investment_account_2)
+print(investment_account_2.get_service_charges())
 print("===================================================")
 
 # 12. Update the balance of each account created in steps 2, 5, 8 and 10 
 # by using the withdraw method of the superclass and withdrawing 
 # the service charges determined by each instance invoking the 
 # polymorphic get_service_charges method.
+try:
+    chequing_account.withdraw(2.5)  
+    savings_account.withdraw(0.5)  
+    investment_account.withdraw(2.5)  
+    investment_account_2.withdraw(0.5)  
 
-
+except Exception as e:
+    print('ERROR:', e)
 
 # 13. Print each of the bank account objects created in steps 2, 5, 8 and 10.
-
+print(chequing_account)
+print(savings_account)
+print(investment_account)
+print(investment_account_2)
